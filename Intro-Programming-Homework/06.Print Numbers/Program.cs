0d00e21cb1de88a0bec3bc39fcc8235da0847af3
Camyul <vankata.lt@outlook.com>
2016-06-27 12:19:22 +0300

    test

--- Intro-Programming-Homework/06.Print Numbers/Program.cs
+++ Intro-Programming-Homework/06.Print Numbers/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main()
         {
-        Console.WriteLine("1");
+        Console.WriteLine(1);
         Console.WriteLine(101);
         Console.WriteLine(1001);
         }
