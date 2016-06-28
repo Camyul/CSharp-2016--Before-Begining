5f28103270bccdf1af7bff1c6a5c955d1a05c6c9
Camyul <vankata.lt@outlook.com>
2016-06-28 19:13:29 +0300

    Complete problem 14

---
+++ Intro-Programming-Homework/14. Current Date and Time/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        DateTime currentTime = DateTime.Now;
+        Console.WriteLine("Data and Time now is {0:dd/MM/yyyy H:mm:ss:fff}", currentTime);
+        }
+    }
