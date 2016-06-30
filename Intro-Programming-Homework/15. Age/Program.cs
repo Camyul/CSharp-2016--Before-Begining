d8637c8fc5b5599be96dfe65c907aad69418b2f6
Camyul <vankata.lt@outlook.com>
2016-06-30 13:25:26 +0300

    Problem 15 - Complete 80/100

---
+++ Intro-Programming-Homework/15. Age/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        //Console.Write("When is your birthday(MM.DD.YYYY): ");
+        DateTime myBirthday = DateTime.Parse(Console.ReadLine());
+        DateTime yearNow = DateTime.Now;
+        int yearsDiff = yearNow.Year - myBirthday.Year;
+        if (yearNow.Month < myBirthday.Month)
+        {
+            yearsDiff--;
+
+        }
+        if (yearNow.Month == myBirthday.Month)
+        {
+            if (yearNow.Day < myBirthday.Day)
+            {
+                yearsDiff--;
+            }
+        }
+        Console.WriteLine(yearsDiff);
+        Console.WriteLine(yearsDiff + 10);
+        }
+    }
