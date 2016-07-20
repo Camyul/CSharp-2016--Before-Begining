0c39eaff20466c9bbd6c42a47c8315850fea67c9
Camyul <vankata.lt@outlook.com>
2016-07-20 10:52:53 +0300

    Problem 4 - Completed

---
+++ 02. Data types and variables/03. Variable in Hexadecimal Format/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        int hexadecimalVariable = 0xFE;
+        Console.WriteLine(hexadecimalVariable);
+        }
+    }
---
+++ 02. Data types and variables/04. Unicode Character/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        char unicodeVariable = '\u002A';
+        Console.WriteLine(unicodeVariable);
+        }
+    }
