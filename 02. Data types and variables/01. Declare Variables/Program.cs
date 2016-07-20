640ccd6cdf75780c6bccf885552a55d7ff7d1722
Camyul <vankata.lt@outlook.com>
2016-07-20 10:42:17 +0300

    Homework 02

---
+++ 02. Data types and variables/01. Declare Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+            ushort ushortVariable = 52130;
+            sbyte sbyteVariable = -115;
+            uint uintVariable = 4825932;
+            byte byteVariable = 97;
+            short shortVariable = -10000;
+        Console.WriteLine("Most suitably Type: \n for {0} is ushort\n for {1} is sbyte\n for {2} is uint\n for {3} is byte\n for {4} is short", ushortVariable, sbyteVariable, uintVariable, byteVariable, shortVariable);
+        }
+    }
