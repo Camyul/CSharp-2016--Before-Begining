bf4bcf6e46d409a9ea90fb7ed7efd1038a5478fb
Camyul <vankata.lt@outlook.com>
2016-07-22 12:55:13 +0300

    Problem 7 - Completed

---
+++ 02. Data types and variables/05. Boolean Variable/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        bool isFemale = new bool();
+        Console.Write("Are You Female(y/n): ");
+        char yourGender = char.Parse(Console.ReadLine());
+        if ((yourGender == 89) || (yourGender == 121))
+        {
+            isFemale = true;
+        }
+        Console.WriteLine("isFemale = {0}", isFemale);
+        }
+    }
---
+++ 02. Data types and variables/06. Strings and Objects/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        string firstWord = "Hello";
+        string secondWord = "World";
+        object objectSentence = firstWord + " " + secondWord;
+        string stringSentence = objectSentence.ToString();
+        Console.WriteLine(stringSentence);
+        }
+    }
