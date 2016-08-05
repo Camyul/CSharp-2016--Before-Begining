7679f80669a46c8a38571cf35fab8953a42f4b31
Camyul <vankata.lt@outlook.com>
2016-08-05 12:10:43 +0300

    Complete problem 09

---
+++ 02. Data types and variables/08. Isosceles Triangle/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        char symbolAt = '\x0040';
+        char symbolSpace = '\u0020';
+        Console.WriteLine("{0}{1}{2}{3}",symbolSpace, symbolSpace, symbolSpace, symbolAt);
+        Console.WriteLine("{0}{1}{2}{3}{4}",symbolSpace, symbolSpace, symbolAt, symbolSpace, symbolAt);
+        Console.WriteLine("{0}{1}{2}{3}{4}{5}", symbolSpace, symbolAt, symbolSpace, symbolSpace, symbolSpace, symbolAt);
+        Console.WriteLine("{0}{1}{2}{3}{4}{5}{6}", symbolAt, symbolSpace, symbolAt, symbolSpace, symbolAt, symbolSpace, symbolAt);
+        }
+    }
---
+++ 02. Data types and variables/09. Exchange Variable Values/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        int a = 5;
+        int b = 10;
+        Console.WriteLine("a = {0}",a);
+        Console.WriteLine("b = {0}",b);
+        b = a + b;
+        a = b - a;
+        b = b - a;
+        Console.WriteLine("a = {0}", a);
+        Console.WriteLine("b = {0}", b);
+    }
+    }
