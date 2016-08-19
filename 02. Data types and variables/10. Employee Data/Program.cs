3b96c95d3d8e62ac2c4e0a834ef00f6f2f564dea
Camyul <vankata.lt@outlook.com>
2016-08-19 12:24:21 +0300

    Complete problem 10

---
+++ 02. Data types and variables/10. Employee Data/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("First Name: ");
+        string nameFirst = Console.ReadLine();
+        Console.Write("Last Name: ");
+        string nameLast = Console.ReadLine();
+        Console.Write("Age: ");
+        byte age = byte.Parse(Console.ReadLine());
+        Console.Write("Personal ID: ");
+        long personalID = long.Parse(Console.ReadLine());
+        int employerNumber = 27560000;        
+        Console.WriteLine(nameFirst);
+        Console.WriteLine(nameLast);
+        Console.WriteLine(age);
+        Console.WriteLine(personalID);
+        Console.WriteLine(employerNumber);
+        employerNumber++;
+    }
+}
