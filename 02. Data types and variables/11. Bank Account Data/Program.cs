b7908e00be8b2a196b7e997426c9f66b6e273a69
Camyul <vankata.lt@outlook.com>
2016-08-23 12:04:53 +0300

    Complete Problem 12

---
+++ 02. Data types and variables/11. Bank Account Data/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("First Name: ");
+        string nameFirst = Console.ReadLine();
+        Console.Write("Middle Name: ");
+        string nameMiddle = Console.ReadLine();
+        Console.Write("Last Name: ");
+        string nameLast = Console.ReadLine();
+        Console.Write("Balance:");
+        decimal moneyBalance = decimal.Parse(Console.ReadLine());
+        Console.Write("Bank Name: ");
+        string nameBank = Console.ReadLine();
+        Console.Write("IBAN: ");
+        string ibanNumber = Console.ReadLine();
+        Console.Write("First Credit Card Number: ");
+        long cardNumber1 = long.Parse(Console.ReadLine());
+        Console.Write("Second Credit Card Number: ");
+        long cardNumber2 = long.Parse(Console.ReadLine());
+        Console.Write("Third Credit Card Number: ");
+        long cardNumber3 = long.Parse(Console.ReadLine());
+        Console.WriteLine("First Name: {0}", nameFirst);
+        Console.WriteLine("Middle Name: {0}", nameMiddle);
+        Console.WriteLine("Last Name: {0}", nameLast);
+        Console.WriteLine("Accaunt Balance: {0}", moneyBalance);
+        Console.WriteLine("Bank Name: {0}", nameBank);
+        Console.WriteLine("IBAN: {0}", nameFirst);
+        Console.WriteLine("First Credit Card Number: {0}", cardNumber1);
+        Console.WriteLine("Second Credit Card Number: {0}", cardNumber2);
+        Console.WriteLine("Third Credit Card Number: {0}", cardNumber3);
+    }
+}
---
+++ 02. Data types and variables/12. Null Values Arithmetic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int? nullableInteger = null;
+        double? nullableDouble = null;
+        int? nullableInteger2 = 5;
+        double? nullableDouble2 = 4.54;
+        Console.WriteLine(nullableInteger + nullableInteger2);
+        Console.WriteLine(nullableDouble + nullableDouble2);
+        Console.WriteLine("{0},{1},{2},{3}", nullableInteger, nullableInteger2, nullableDouble, nullableDouble2);
+    }
+}
