1a133d610f948d8640e29eddd91d2226fc1aa93c
Camyul <vankata.lt@outlook.com>
2016-08-24 12:04:35 +0300

    Complete problem 14

---
+++ 02. Data types and variables/13. Comparing Floats/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        bool isEqual = false;
+        if ((Math.Abs(a - b)) < 0.000001)
+        {
+            isEqual = true;
+        }
+        Console.WriteLine(isEqual);
+    }
+}
---
+++ 02. Data types and variables/14. Print the ASCII Table/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 256; i++)
+        {
+            if  ((i == 95) | (i == 190))
+            {
+                Console.WriteLine();
+            }
+            Console.Write((char)i);
+        }
+    }
+}
