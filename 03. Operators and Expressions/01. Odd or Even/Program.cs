f492c08f20c9f59d129e3a76c77ee6c848dafcf4
Camyul <vankata.lt@outlook.com>
2016-08-26 11:19:01 +0300

    Complete problem 2

---
+++ 03. Operators and Expressions/01. Odd or Even/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        sbyte number = Convert.ToSByte(Console.ReadLine());
+        sbyte rest = (sbyte)(number % 2);
+        if (rest == 0)
+        {
+            Console.WriteLine("even {0}", number);
+        }
+        else
+        {
+            Console.WriteLine("odd {0}", number);
+        }
+    }
+}
---
+++ 03. Operators and Expressions/02. Moon Gravity/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double weight = Convert.ToDouble(Console.ReadLine());
+        double moonWeight = 0.17 * weight;
+        Console.WriteLine("{0:N3}", moonWeight);
+    }
+}
