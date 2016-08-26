43e94e51dcb8ecda355e597f4d531de4959f54b4
Camyul <vankata.lt@outlook.com>
2016-08-26 12:19:19 +0300

    Complete problem 4

---
+++ 03. Operators and Expressions/04. Rectangles/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double rectangleWidth = Convert.ToDouble(Console.ReadLine());
+        double rectangleHeight = Convert.ToDouble(Console.ReadLine());
+        double areaRectangle = rectangleHeight * rectangleWidth;
+        double perimeterRectangle = (2 * rectangleWidth) + (2 * rectangleHeight);
+        Console.WriteLine("{0:F2} {1:F2}", areaRectangle, perimeterRectangle);
+    }
+}
