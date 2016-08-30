a605f53fa4dff4896ceda1a6d79110d62265c441
Camyul <vankata.lt@outlook.com>
2016-08-30 12:01:38 +0300

    Problem 5, complete

---
+++ 03. Operators and Expressions/05. Third digit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        number /= 100;
+        number %= 10;
+        if (number == 7)
+        {
+            Console.WriteLine("true");
+        }
+        else
+        {
+            Console.WriteLine("false {0}", number);
+        }
+    }
+}
