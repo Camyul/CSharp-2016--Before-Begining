367553d56b120bec8b25796d4223f555a7d8ab67
Camyul <vankata.lt@outlook.com>
2016-09-02 22:15:21 +0300

    Complete Problem 7

---
+++ 03. Operators and Expressions/07. Point in a circle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        double coordinateX = Convert.ToDouble(Console.ReadLine());
+        double coordinateY = double.Parse(Console.ReadLine());
+        double radius = Math.Sqrt((Math.Pow(coordinateX, 2)) + (Math.Pow(coordinateY, 2)));
+        if (radius <= 2) 
+        {
+            if (radius >= -2)
+            {
+                Console.WriteLine("yes {0:F2}", radius);
+            }
+        }
+        else
+        {
+            Console.WriteLine("no {0:F2}", radius);
+        }
+    }
+}
