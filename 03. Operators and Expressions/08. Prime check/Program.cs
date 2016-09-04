0da3c5ae16419f65b8ce0f4b344c7557c1d0a4da
Camyul <vankata.lt@outlook.com>
2016-09-04 19:22:21 +0300

    Complete Problem 8

--- 03. Operators and Expressions/08. Prime check/Program.cs
+++ 03. Operators and Expressions/08. Prime check/Program.cs
@@ -1,14 +1,24 @@
 ﻿using System;
-
-
 class Program
 {
     static void Main()
     {
         int number = Convert.ToInt32(Console.ReadLine());
-        if (((number % 2) == 0))
+        bool isPrime = true;
+        if (number < 2)
         {
-
+            Console.WriteLine(!isPrime);
+        }
+        else
+        {
+            for (int i = 2; i <= number; i++)
+            {
+                if (((number % i) == 0) && (number > i))
+                {
+                    isPrime = false;
+                }
+            }
+            Console.WriteLine(isPrime);
         }
     }
 }
