2070d5f60755163f7f2b5e27a0efc024f7acaf4b
Camyul <vankata.lt@outlook.com>
2016-09-04 17:11:51 +0300

    Problem 8

---
+++ 03. Operators and Expressions/08. Prime check/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (((number % 2) == 0))
+        {
+
+        }
+    }
+}
