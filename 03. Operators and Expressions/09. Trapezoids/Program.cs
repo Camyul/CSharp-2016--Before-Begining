df33dcd154c3f4234c1671d8a239a58c8b3eebff
Camyul <vankata.lt@outlook.com>
2016-09-04 20:06:39 +0300

    problem 9

--- 03. Operators and Expressions/09. Trapezoids/Program.cs
+++ 03. Operators and Expressions/09. Trapezoids/Program.cs
@@ -10,6 +10,11 @@ namespace _09.Trapezoids
     {
         static void Main(string[] args)
         {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double areaTrapezoid = (((a + b) / 2) * h);
+            Console.WriteLine("{0:F7}", areaTrapezoid);
         }
     }
 }
