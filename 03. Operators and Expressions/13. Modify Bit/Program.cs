645aa4478cc8d835bb1b37fc9c385b6de6ad6cb5
Camyul <vankata.lt@outlook.com>
2016-09-06 18:20:08 +0300

    Complete problem 14

---
+++ 03. Operators and Expressions/13. Modify Bit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        uint n = uint.Parse(Console.ReadLine());
+        int p = int.Parse(Console.ReadLine());
+        int v = int.Parse(Console.ReadLine());
+        if (v == 0)
+        {
+            int mask = ~(1 << p);
+            n = n & (uint)mask;
+        }
+        else
+        {
+            int mask = 1 << p;
+            n = n | (uint)mask;
+        }
+        Console.WriteLine(n);
+    }
+}
---
+++ 03. Operators and Expressions/14. BitExchange/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        uint n = Convert.ToUInt32(Console.ReadLine());
+        uint[] p = new uint[6];
+        int mask = new int();
+        for (int i = 0; i <= 2; i++)
+        {
+            mask = 1 << (i + 3);
+            p[i] = (n & (uint)mask) >> (i + 3);
+        }
+        for (int i = 3; i <= 5; i++)
+        {
+            mask = 1 << (i + 21);
+            p[i] = (n & (uint)mask) >> (i + 21);
+        }
+        for (int i = 0; i <= 2; i++)
+        {
+            if (p[i] == 0)
+            {
+                mask = ~(1 << (i + 24));
+                n = n & (uint)mask;
+            }
+            else
+            {
+                mask = 1 << (i + 24);
+                n = n | (uint)mask;
+            }
+        }
+        for (int i = 3; i <= 5; i++)
+        {
+            if (p[i] == 0)
+            {
+                mask = ~(1 << i);
+                n = n & (uint)mask;
+            }
+            else
+            {
+                mask = 1 << i;
+                n = n | (uint)mask;
+            }
+        }
+        Console.WriteLine(n);
+    }
+}
