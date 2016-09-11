39eb7d34ce41e2f51f1ee4d27453201239e3a704
Camyul <vankata.lt@outlook.com>
2016-09-11 11:17:32 +0300

    Complete Problem 4.2

---
+++ 04. Console-In-and-Out/02. Company info/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        string companyName = Console.ReadLine();
+        string companyAdress = Console.ReadLine();
+        string phoneNumber = Console.ReadLine();
+        string faxNumber = Console.ReadLine();
+        string webSait = Console.ReadLine();
+        string managerFirstName = Console.ReadLine();
+        string managerLastName = Console.ReadLine();
+        string managerAge = Console.ReadLine();
+        string managerPhoneNumber = Console.ReadLine();
+        if (faxNumber.Length == 0)
+        {
+            faxNumber = "(no fax)";
+        }
+        Console.WriteLine(companyName);
+        Console.WriteLine("Address: {0}", companyAdress);
+        Console.WriteLine("Tel. {0}", phoneNumber);
+        Console.WriteLine("Fax: {0}", faxNumber);
+        Console.WriteLine("Web site: {0}", webSait);
+        Console.Write("Manager: {0} {1}", managerFirstName, managerLastName);
+        Console.WriteLine(" (age: {0}, tel. {1})", managerAge, managerPhoneNumber);
+    }
+}
