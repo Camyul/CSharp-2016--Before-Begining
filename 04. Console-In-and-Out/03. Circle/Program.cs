192120c7f1309b84e166cfea0341098304e9c3c1
Camyul <vankata.lt@outlook.com>
2016-09-12 13:27:24 +0300

    Complete Problem 4.6

---
+++ 04. Console-In-and-Out/03. Circle/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double radius = double.Parse(Console.ReadLine());
+        Console.WriteLine("{0:F2} {1:0.00}", (2 * radius) * (Math.PI), (Math.PI) * (Math.Pow(radius, 2)));
+    }
+}
---
+++ 04. Console-In-and-Out/05. Number Comparer/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double A = double.Parse(Console.ReadLine());
+        double B = double.Parse(Console.ReadLine());
+        Console.WriteLine(Math.Max(A,B));
+    }
+}
