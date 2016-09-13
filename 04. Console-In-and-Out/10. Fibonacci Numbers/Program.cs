65645c4e07949405f5996f39d8fcdf23226df353
Camyul <vankata.lt@outlook.com>
2016-09-13 11:11:13 +0300

    Complete Problem 10

---
+++ 04. Console-In-and-Out/10. Fibonacci Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        long fibonacciOne = 0;
+        long fibonacciTwo = 1;
+        long fibonacciSum = 1;
+        Console.Write(fibonacciOne);
+        if (n >= 2)
+        {
+            Console.Write(", {0}", fibonacciTwo);
+        }
+        if (n > 2)
+        {
+            for (int i = 2; i < n; i++)
+            {
+                Console.Write(", {0}", fibonacciSum);
+                fibonacciOne = fibonacciTwo;
+                fibonacciTwo = fibonacciSum;
+                fibonacciSum = fibonacciOne + fibonacciTwo;
+            }
+        }               
+    }
+}
