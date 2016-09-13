0b4904a87a19225bb7efbb893b23fb0903c0cbd0
Camyul <vankata.lt@outlook.com>
2016-09-13 12:19:20 +0300

    Complete Problem 4.11

---
+++ 04. Console-In-and-Out/11. Interval/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        short n = short.Parse(Console.ReadLine());
+        short m = short.Parse(Console.ReadLine());
+        byte count = 0;
+        for (int i = n; i < m; i++)
+        {
+            if (((i % 5) == 0) && (i != n) && (i != m))
+            {
+                count++;
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
