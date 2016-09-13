59545ee1525af139433897f19e2e81c8314cb5e1
Camyul <vankata.lt@outlook.com>
2016-09-13 10:22:10 +0300

    Complete Problem 4.9

--- 04. Console-In-and-Out/01. Sum of 3 numbers/Program.cs
+++ 04. Console-In-and-Out/01. Sum of 3 numbers/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main()
     {
-        double a = double.Parse(Console.ReadLine());
-        double b = double.Parse(Console.ReadLine());
-        double c = double.Parse(Console.ReadLine());
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
         Console.WriteLine(a + b + c);
     }
 }
---
+++ 04. Console-In-and-Out/07. Sum of 5 numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        int number3 = Convert.ToInt32(Console.ReadLine());
+        int number4 = Convert.ToInt32(Console.ReadLine());
+        int number5 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(number1+number2+number3+number4+number5);
+    }
+}
