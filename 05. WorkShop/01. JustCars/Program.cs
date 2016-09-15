a4e6bca125c57785db0dd3d2e91fd4fdc0a38bc2
Camyul <vankata.lt@outlook.com>
2016-09-15 13:11:41 +0300

    Just Cars 5.1 Completed

--- 05. WorkShop/01. JustCars/Program.cs
+++ 05. WorkShop/01. JustCars/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-struct Car
+struct Object
 {
     public int x;
     public int y;
@@ -22,33 +22,71 @@ class Program
         Console.ForegroundColor = color;
         Console.WriteLine(c);
     }
+    static void PrintStringOnPosition(int x, int y, string str, ConsoleColor color = ConsoleColor.Gray)
+    {
+        Console.SetCursorPosition(x, y);
+        Console.ForegroundColor = color;
+        Console.WriteLine(str);
+    }
     static void Main()
     {
         int playerfieldWidth = 5;
-        Console.BufferHeight = Console.WindowHeight = 20;
+        int livescount = 5;
+        double speed = 400;
+        double acceleration = 0.5;
+        Console.BufferHeight = Console.WindowHeight = 30;
         Console.BufferWidth = Console.WindowWidth = 30;
-        Car userCar = new Car();
+        Object userCar = new Object();
         userCar.x = 2;
         userCar.y = Console.WindowHeight - 1;
         userCar.c = '@';
         userCar.color = ConsoleColor.Yellow;
         Random randomGenerator = new Random();
-        List<Car> cars = new List<Car>();
+        List<Object> objects = new List<Object>();
         while (true)
         {
+            speed += acceleration;
+            if (speed > 500)
+            {
+                speed = 500;
+            }
+            bool hitted = false;
             //Move our car (key passed)
-            Car newCar = new Car();
-            newCar.color = ConsoleColor.DarkGreen;
-            newCar.x = randomGenerator.Next(0, playerfieldWidth);
-            newCar.y = 0;
-            newCar.c = '#';
-            if (Console.KeyAvailable)
+            int chanse = randomGenerator.Next(0, 100);
+            if (chanse < 2)
+            {
+                Object bonusSpeed = new Object();
+                bonusSpeed.c = '-';
+                bonusSpeed.color = ConsoleColor.Cyan;
+                bonusSpeed.x = randomGenerator.Next(0, playerfieldWidth);
+                bonusSpeed.y = 0;
+                objects.Add(bonusSpeed);
+            }
+            else if (chanse < 20)
+            {
+                Object bonusSpeed = new Object();
+                bonusSpeed.c = '*';
+                bonusSpeed.color = ConsoleColor.Cyan;
+                bonusSpeed.x = randomGenerator.Next(0, playerfieldWidth);
+                bonusSpeed.y = 0;
+                objects.Add(bonusSpeed);
+            }
+            else
+            {
+                Object newCar = new Object();
+                newCar.color = ConsoleColor.DarkGreen;
+                newCar.x = randomGenerator.Next(0, playerfieldWidth);
+                newCar.y = 0;
+                newCar.c = '#';
+                objects.Add(newCar);
+            }
+            while (Console.KeyAvailable)
             {
                 ConsoleKeyInfo pressedKey = Console.ReadKey(true);
-                while (Console.KeyAvailable)
-                {
-                    Console.ReadKey(true);
-                }
+                //while (Console.KeyAvailable)
+                //{
+                //    Console.ReadKey(true);
+                //}
                 if (pressedKey.Key == ConsoleKey.LeftArrow)
                 {
                     if (userCar.x > 0)
@@ -64,17 +102,67 @@ class Program
                     }
                 }
             }
-                        
             //Move Cars
+            List<Object> newList = new List<Object>();
+            for (int i = 0; i < objects.Count; i++)
+            {
+                Object oldCar = objects[i];
+                Object newObject = new Object();
+                newObject.x = oldCar.x;
+                newObject.y = oldCar.y + 1;
+                newObject.c = oldCar.c;
+                newObject.color = oldCar.color;
+                if ((newObject.c == '-') && (newObject.y == userCar.y) && (newObject.x == userCar.x))
+                {
+                    livescount++;
+                }
+                if ((newObject.c == '*') && (newObject.y == userCar.y) && (newObject.x == userCar.x))
+                {
+                    speed -= 20;
+                }
+                if ((newObject.c == '#') && (newObject.y == userCar.y) && (newObject.x == userCar.x))
+                {
+                    livescount--;
+                    hitted = true;
+                    speed += 50;
+                }
+                if (newObject.y < Console.WindowHeight)
+                {
+                    newList.Add(newObject);
+                }
+                             
+            }
+            objects = newList;
             //Check if other cars hitting us
             //Clear the console
             Console.Clear();
             //Redraw Playfield
-            PrintOnPosition(userCar.x, userCar.y, userCar.c, userCar.color);
-
+            foreach (Object car in objects)
+            {
+                PrintOnPosition(car.x, car.y, car.c, car.color);
+            }
+            if (hitted == true)
+            {
+                objects.Clear();
+                PrintOnPosition(userCar.x, userCar.y, 'X', ConsoleColor.Red);
+            }
+            else
+            {
+                PrintOnPosition(userCar.x, userCar.y, userCar.c, userCar.color);
+            }
+            if (livescount < 0)
+            {
+                PrintStringOnPosition(8, 10, "GAME OVER!!!", ConsoleColor.Red);
+                PrintStringOnPosition(8, 12, "Press [enter] to exit", ConsoleColor.Red);
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
             //Draw info
-            //Slow Daown Program
-            Thread.Sleep(100);
+            PrintStringOnPosition(8,4,"Lives: "+livescount, ConsoleColor.White);
+            PrintStringOnPosition(8,5,"Speed: "+speed, ConsoleColor.White);
+            PrintStringOnPosition(8,6,"Acceleration: " + acceleration, ConsoleColor.White);
+            //Slow Dawn Program
+            Thread.Sleep((int)(600-speed));
         }
     }
 }
