21d5b8d9493d64a7e2b44f9f30244dd5eda00df2
Camyul <vankata.lt@outlook.com>
2016-09-16 15:35:49 +0300

    Falling Rock - UserPad Move to left

--- 05. WorkShop/02. Falling Rocks/Program.cs
+++ 05. WorkShop/02. Falling Rocks/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+
 class Program
 {
     struct Rock
@@ -8,7 +10,7 @@ class Program
         public string str;
         public ConsoleColor color;
     }
-    static void PrintStringOnPosition(int x, int y, string str, ConsoleColor color = ConsoleColor.Gray)
+    static void PrintStringOnPosition(int x, int y, string str, ConsoleColor color)
     {
         Console.SetCursorPosition(x, y);
         Console.ForegroundColor = color;
@@ -23,14 +25,29 @@ class Program
         userPad.color = ConsoleColor.White;
         Console.BufferWidth = Console.WindowWidth = 60;
         Console.BufferHeight = Console.WindowHeight = 30;
-        //Move our object
-        //Move rocks
-        //Check if rocks hitting us
-        //Clear the console
-        Console.Clear();
-        //Redraw rocks
-        PrintStringOnPosition(userPad.x, userPad.y, userPad.str, userPad.color);
-        //Draw info
-        //Slow down program
+        while (true)
+        {
+            //Move our object
+            while (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+                if (pressedKey.Key == ConsoleKey.LeftArrow)
+                {
+                    if (userPad.x > 0)
+                    {
+                        userPad.x--;
+                    }
+                }
+            }
+            //Move rocks
+            //Check if rocks hitting us
+            //Clear the console
+            Console.Clear();
+            //Redraw rocks
+            PrintStringOnPosition(userPad.x, userPad.y, userPad.str, userPad.color);
+            //Draw info
+            //Slow down program
+            Thread.Sleep(200);
+        }
     }
 }
