e36043af166339b8e060c78dea7b22e8d89034ec
Camyul <vankata.lt@outlook.com>
2016-09-17 19:20:08 +0300

    05.02 The Rocks falling

--- 05. WorkShop/02. Falling Rocks/Program.cs
+++ 05. WorkShop/02. Falling Rocks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 class Program
@@ -18,15 +19,25 @@ class Program
     }
     static void Main()
     {
+        
+        Console.BufferWidth = Console.WindowWidth = 60;
+        Console.BufferHeight = Console.WindowHeight = 30;
         Rock userPad = new Rock();
         userPad.x = 30;
         userPad.y = Console.WindowHeight - 1;
         userPad.str = "(0)";
         userPad.color = ConsoleColor.White;
-        Console.BufferWidth = Console.WindowWidth = 60;
-        Console.BufferHeight = Console.WindowHeight = 30;
+        Random randomGenerator = new Random();
+        List<Rock> fallingRocks = new List<Rock>();
         while (true)
         {
+            
+            Rock newRock = new Rock();
+            newRock.x = randomGenerator.Next(0, Console.WindowWidth);
+            newRock.y = 1;
+            newRock.str = "%";
+            newRock.color = ConsoleColor.Red;
+            fallingRocks.Add(newRock);
             //Move our object
             while (Console.KeyAvailable)
             {
@@ -38,12 +49,39 @@ class Program
                         userPad.x--;
                     }
                 }
+                if (pressedKey.Key == ConsoleKey.RightArrow)
+                {
+                    if (userPad.x < (Console.BufferWidth - 4))
+                    {
+                        userPad.x++;
+                    }
+
+                }
             }
             //Move rocks
+            List <Rock> tempRock = new List<Rock>();
+            for (int i = 0; i < fallingRocks.Count; i++)
+            {
+                Rock oldRock = fallingRocks[i];
+                Rock moveRock = new Rock();
+                moveRock.x = oldRock.x;
+                moveRock.y = oldRock.y + 1;
+                moveRock.str = oldRock.str;
+                moveRock.color = oldRock.color;
+                if (moveRock.y < Console.WindowHeight)
+                {
+                    tempRock.Add(moveRock);
+                }
+            }
+            fallingRocks = tempRock;
             //Check if rocks hitting us
             //Clear the console
             Console.Clear();
-            //Redraw rocks
+            //Redraw playerfield
+            foreach (Rock Rok in fallingRocks)
+            {
+                PrintStringOnPosition(Rok.x, Rok.y, Rok.str, Rok.color);
+            }
             PrintStringOnPosition(userPad.x, userPad.y, userPad.str, userPad.color);
             //Draw info
             //Slow down program
