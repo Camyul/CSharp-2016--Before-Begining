76edad0583d3417a6d69c0b0c23602f9b93d21a3
Camyul <vankata.lt@outlook.com>
2016-09-22 02:41:31 +0300

    06.03 Problem Completed

---
+++ 06. Conditional-Statements/06.01 Exchange numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        if (b < a)
+        {
+            double c = b;
+            b = a;
+            a = c;
+        }
+        Console.WriteLine("{0} {1}", a, b);
+    }
+}
