8d71a2fe1690499e265b0fcfda22c1e22520816d
Camyul <vankata.lt@outlook.com>
2016-09-24 11:37:35 +0300

    Complete Problem 06.09

---
+++ 06. Conditional-Statements/06.09 Int, Double, String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string typeVariable = Console.ReadLine();
+        if (typeVariable == "integer")
+        {
+            int intNumber = Convert.ToInt32(Console.ReadLine());
+            intNumber++;
+            Console.WriteLine(intNumber);
+        }
+        else if (typeVariable == "real")
+        {
+            double doubleNumber = Convert.ToDouble(Console.ReadLine());
+            doubleNumber += 1;
+            Console.WriteLine("{0:F2}", doubleNumber);
+        }
+        else
+        {
+            string frase = Console.ReadLine();
+            Console.WriteLine(frase + "*");
+        }
+    }
+}
