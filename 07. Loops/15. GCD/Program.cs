531dfdb69887a0dff6b277e6ec4bf0edcbaaf043
Camyul <vankata.lt@outlook.com>
2016-10-06 15:44:54 +0300

    Working on problem 07.15

--- 07. Loops/15. GCD/Program.cs
+++ 07. Loops/15. GCD/Program.cs
@@ -8,10 +8,22 @@ class Program
         string twoNumbers = Console.ReadLine();
         string[] separatedNumbers = twoNumbers.Split(' ');
         int[] intNumbers = new int[separatedNumbers.Length];
+        int firstNumber = new int();
+        int secondNumber = new int();
+        int rest = 2;
         for (int i = 0; i < separatedNumbers.Length; i++)
         {
             intNumbers[i] = Convert.ToInt32(separatedNumbers[i]);
         }
-        Console.WriteLine(intNumbers[0]);
+        firstNumber = Math.Max(intNumbers[0], intNumbers[1]);
+        secondNumber = Math.Min(intNumbers[0], intNumbers[1]);
+        do
+        {
+            rest = firstNumber % secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = rest;
+        }
+        while (rest > 1);
+        Console.WriteLine(secondNumber);
     }
 }
