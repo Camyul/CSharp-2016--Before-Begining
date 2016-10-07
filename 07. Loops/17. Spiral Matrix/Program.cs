99484a74b3e3ca347712794efe1d1d484572ebca
Camyul <vankata.lt@outlook.com>
2016-10-07 10:08:24 +0300

    Working on 07.17

--- 07. Loops/17. Spiral Matrix/Program.cs
+++ 07. Loops/17. Spiral Matrix/Program.cs
@@ -3,8 +3,32 @@
 
 class Program
 {
+    static void PrintOnPosition(int x, int y, int symbol)
+    {
+        Console.SetCursorPosition(x, y);
+        Console.WriteLine(symbol);
+    }
     static void Main()
     {
-
+        int n = Convert.ToInt32(Console.ReadLine());
+        int matrixLength = n * n;
+        int[] matrixN = new int[matrixLength];
+        for (int i = 0, j = 1; i < matrixLength; i++, j++)
+        {
+            matrixN[i] = j;
+        }
+        int count = 0;
+        while (count < matrixLength)
+        {
+            int x = 0;
+            int y = 0;
+            while (count < n)
+            {
+                PrintOnPosition(x, y, matrixN[count]);
+                x++;
+                count++;
+            }
+        }
+        //PrintOnPosition(10, 12, 33);
     }
 }
