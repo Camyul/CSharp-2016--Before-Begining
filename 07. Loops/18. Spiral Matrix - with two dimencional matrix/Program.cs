cd71d89f28172fe53090a5b17500cfd26faa5901
Camyul <vankata.lt@outlook.com>
2016-10-08 22:57:10 +0300

    Work

---
+++ 07. Loops/18. Spiral Matrix - with two dimencional matrix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.Clear();
+        int matrixLength = n * n;
+        int[,] matrixN = new int[n,n];
+        int count = 1;
+        int col = 0;
+        int row = 0;
+        string direction = "right";
+        for (int i = 1; i <= matrixLength; i++)
+        {
+            if ((direction == "right") && (col > n))
+            {
+                direction = "down";
+                col--;
+            }
+            matrixN[row, col] = i;
+            if (direction == "right")
+            {
+                col++;
+            }
+        }
+        
+  
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrixN[i,j]);
+            }
+        }
+    }
+}
