ad570f6805b82cb0435411e225f8b74a213f8f45
Camyul <vankata.lt@outlook.com>
2016-10-09 12:52:09 +0300

    Complete Problem 07.17

--- 07. Loops/18. Spiral Matrix - with two dimencional matrix/Program.cs
+++ 07. Loops/18. Spiral Matrix - with two dimencional matrix/Program.cs
@@ -7,33 +7,64 @@ class Program
     {
         int n = Convert.ToInt32(Console.ReadLine());
         Console.Clear();
-        int matrixLength = n * n;
         int[,] matrixN = new int[n,n];
-        int count = 1;
         int col = 0;
         int row = 0;
         string direction = "right";
-        for (int i = 1; i <= matrixLength; i++)
+        for (int i = 1; i <= (n*n); i++)
         {
-            if ((direction == "right") && (col > n))
+            if ((direction == "right") && ((col >= n) || (matrixN[row,col] != 0)))
             {
                 direction = "down";
                 col--;
+                row++;
             }
+            if ((direction == "down") && ((row >= n) || (matrixN[row, col] != 0)))
+            {
+                direction = "left";
+                row--;
+                col--;
+            }
+            if ((direction == "left") && ((col < 0) || (matrixN[row, col] != 0)))
+            {
+                direction = "up";
+                col++;
+                row--;
+            }
+            if ((direction == "up") && ((row <= 0) || (matrixN[row, col] != 0)))
+            {
+                direction = "right";
+                row++;
+                col++;
+            }
+
             matrixN[row, col] = i;
+
             if (direction == "right")
             {
                 col++;
             }
+            if (direction == "down")
+            {
+                row++;
+            }
+            if (direction == "left")
+            {
+                col--;
+            }
+            if (direction == "up")
+            {
+                row--;
+            }
         }
         
-  
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                Console.Write(matrixN[i,j]);
+                Console.Write("{0,4}",matrixN[i,j]);
             }
+            Console.WriteLine();
         }
     }
 }
