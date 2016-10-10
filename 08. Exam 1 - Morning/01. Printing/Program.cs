73c89c455e493fcd2933125fc62dc5ecbd8659d6
Camyul <vankata.lt@outlook.com>
2016-10-10 22:33:16 +0300

    08.02 Complete

---
+++ 08. Exam 1 - Morning/01. Printing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int s = int.Parse(Console.ReadLine());
+        double p = double.Parse(Console.ReadLine());
+        double realm = (n * s) / 500d;
+        Console.WriteLine("{0:F2}", realm * p);
+    }
+}
---
+++ 08. Exam 1 - Morning/02. Text to Number/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int m = Convert.ToInt32(Console.ReadLine());
+        string textLine = Console.ReadLine();
+        char[] simbols = new char[textLine.Length];
+        long result = 0;
+        for (int i = 0; i < textLine.Length ; i++)
+        {
+            simbols[i] = Convert.ToChar(textLine.Substring(i, 1));
+        }
+        for (int i = 0; i < textLine.Length; i++)
+        {
+            if (simbols[i] == '@')
+            {
+                break;
+            }
+            else if ((int)simbols[i] > 47 && (int)simbols[i] < 58)
+            {
+                result *= (long)char.GetNumericValue(simbols[i]);
+            }
+            else if ((int)simbols[i] > 64 && (int)simbols[i] < 91)
+            {
+                result += (int)simbols[i] - 65;
+            }
+            else if ((int)simbols[i] > 96 && (int)simbols[i] < 123)
+            {
+                result += (int)simbols[i] - 97;
+            }
+            else
+            {
+                result = result % m;
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
