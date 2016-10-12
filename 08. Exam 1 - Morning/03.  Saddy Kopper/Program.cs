b4ada9693ccee739679cafdcdc43a884efea4bfb
Camyul <vankata.lt@outlook.com>
2016-10-12 15:27:31 +0300

    Working on Problem 08.03

---
+++ 08. Exam 1 - Morning/03.  Saddy Kopper/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        BigInteger number = BigInteger.Parse(Console.ReadLine());
+        int sum = 0;
+        int count = 0;
+        int[] sums = new int(16);
+        string numberToString = "";
+        while (number > 9 && count <10)
+        {
+            number /= 10;
+            numberToString = Convert.ToString(number);
+            for (int i = 0; i < numberToString.Length; i+=2)
+            {
+                sum += Convert.ToInt32(numberToString.Substring(i, 1));
+            }
+            count++;
+            if (count > 9)
+            {
+                break;
+            }
+        }
+        Console.WriteLine(sum);
+        Console.WriteLine(count);
+    }
+}
