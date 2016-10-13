b82db03e2fcd3b2dff4b82a0d1afb47f53360f78
Camyul <vankata.lt@outlook.com>
2016-10-13 12:01:06 +0300

    Working

---
+++ 08. Exam 1 - Morning/04. Cube/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        //int n = int.Parse(Console.ReadLine());
+        //int row = 0;
+        //char[] simbolCube = { '9',':', '/', 'X' };
+        //if (row == 0)
+        //{
+        //    for (int j = 0; j <= row; j++)
+        //    {
+        //        for (int i = 0; i < n - row; i++)
+        //        {
+        //            Console.Write(simbolCube[j]);
+        //        }
+        //        Console.Write(simbolCube[j+1-row]);
+        //        if (j==1)
+        //        {
+        //            break;
+        //        }
+        //        row++;
+        //    }
+        //}
+        
+        Console.Write(new string('y', 10));
+
+    }
+}
