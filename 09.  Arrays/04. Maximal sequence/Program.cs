ed4afae0e292ccddb347a2e4e84e661241e543e2
Camyul <vankata.lt@outlook.com>
2016-10-21 00:59:13 +0300

    Complete Problem 09.04

---
+++ 09.  Arrays/04. Maximal sequence/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] numbers = new int[n];
+        int count = 1;
+        int maxCount = 0; ;
+        for (int i = 0; i < n; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine()); 
+        }
+        for (int i = 1; i < n; i++)
+        {
+            if (numbers[i] == numbers[i-1])
+            {
+                count++;
+                
+            }
+            else
+            {
+                if (maxCount <= count)
+                {
+                    maxCount = count;
+                }
+                count = 1;
+            }
+        }
+        Console.WriteLine(maxCount);
+    }
+}
